f807a747f93bf20b682410c409b8964c6d6580ea
rigby200602 <tungle3456@gmail.com>
2025-09-06 22:54:53 +0700

    build navbar

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='text-white'>NavBar</div>
+    <div className='flex text-white mx-[5%] my-[2%]'>
+        <h1 className='text-4xl font-bold'>FenFilm</h1>
+        <div className='ml-auto'>
+            <button className='rounded-4xl cursor-pointer px-4 py-2 hover:bg-white hover:text-[#17252A]'>
+                Ủng hộ tôi
+            </button>
+        </div>
+    </div>
   )
 }
 
