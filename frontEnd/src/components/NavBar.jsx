8e071511cc4bc7c26ea0dc62857bd9a46cf746ef
rigby200602 <tungle3456@gmail.com>
2025-09-06 22:57:20 +0700

    update small css

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='flex text-white mx-[5%] my-[2%]'>
-        <h1 className='text-4xl font-bold'>FenFilm</h1>
+        <h1 className='text-4xl font-bold cursor-pointer'>FenFilm</h1>
         <div className='ml-auto'>
             <button className='rounded-4xl cursor-pointer px-4 py-2 hover:bg-white hover:text-[#17252A]'>
                 Ủng hộ tôi
