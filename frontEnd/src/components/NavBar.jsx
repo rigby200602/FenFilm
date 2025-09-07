034a7e7460df594295feda3f85fc1aa99dc5e1b6
rigby200602 <tungle3456@gmail.com>
2025-09-07 16:06:04 +0700

    build white line, now to config it css

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div className='flex text-white mx-[5%] mt-4'>
-        <h1 className='text-4xl font-bold cursor-pointer'>FenFilm</h1>
-        <div className='ml-auto'>
-            <button className='rounded-4xl cursor-pointer px-4 py-2 hover:bg-white hover:text-[#17252A]'>
-                Ủng hộ tôi
-            </button>
+    <>
+      <div className="flex text-white mx-[5%] mt-4">
+        <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
+        <div className="ml-auto">
+          <button className="rounded-4xl cursor-pointer px-4 py-2 hover:bg-white hover:text-[#17252A]">
+            Ủng hộ tôi
+          </button>
         </div>
-        <br className='border-b-white'/>
-    </div>
-  )
-}
+      </div>
+      <div className="w-full bg-white h-[1px] mt-4">
+        <br />
+      </div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
