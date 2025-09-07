2a82187a4f9ce58aacf637ade594bd89f5ff4b20
rigby200602 <tungle3456@gmail.com>
2025-09-07 23:22:01 +0700

    blueprint footer

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import fb from "../assets/fb.png";
+import tiktok from "../assets/tiktok.png";
 
 const Footer = () => {
   return (
     <div className="absolute bottom-0 text-white h-[25%] w-full px-[5%]">
       <div className="bg-white h-[1px] my-4"></div>
-      <div className="flex flex-col justify-center w-[25%]">
+      <div className="flex w-full h-[75%]">
+        <div className="flex flex-col justify-center items-center w-[25%]">
         <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
-        <div className="mt-4">
-          <img src={fb} alt="Facebook" width="50px" height="50px" />
-          <img />
+        <div className="flex mt-4">
+          <img src={fb} alt="Facebook" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+          <img src={tiktok} alt="Tik Tok" width="40px" height="40px" className="mx-4 cursor-pointer"/>
         </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-[25%]">
+        <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
+        <div className="flex mt-4">
+          <img src={fb} alt="Facebook" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+          <img src={tiktok} alt="Tik Tok" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-[25%]">
+        <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
+        <div className="flex mt-4">
+          <img src={fb} alt="Facebook" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+          <img src={tiktok} alt="Tik Tok" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-[25%]">
+        <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
+        <div className="flex mt-4">
+          <img src={fb} alt="Facebook" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+          <img src={tiktok} alt="Tik Tok" width="40px" height="40px" className="mx-4 cursor-pointer"/>
+        </div>
+      </div>
       </div>
     </div>
   );
