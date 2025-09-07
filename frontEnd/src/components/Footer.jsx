e3ff39b4b157d22389b6cfe99ba586d86feac7bd
rigby200602 <tungle3456@gmail.com>
2025-09-07 22:56:52 +0700

    add fb icon

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import fb from "../assets/fb.png";
 
 const Footer = () => {
   return (
     <div className="absolute bottom-0 text-white h-[25%] w-full px-[5%]">
       <div className="bg-white h-[1px] my-4"></div>
-      <h1 className="text-end text-4xl font-bold cursor-pointer">FenFilm</h1>
+      <div className="flex flex-col justify-center w-[25%]">
+        <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
+        <div className="mt-4">
+          <img src={fb} alt="Facebook" width="50px" height="50px" />
+          <img />
+        </div>
+      </div>
     </div>
   );
 };
