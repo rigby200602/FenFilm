6b357a4c4c2e5fb51b3a99eeb7d1fb6190782434
rigby200602 <tungle3456@gmail.com>
2025-09-07 15:55:41 +0700

    build underline

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex text-white mx-[5%] my-[2%]'>
+    <div className='flex text-white mx-[5%] mt-4'>
         <h1 className='text-4xl font-bold cursor-pointer'>FenFilm</h1>
         <div className='ml-auto'>
             <button className='rounded-4xl cursor-pointer px-4 py-2 hover:bg-white hover:text-[#17252A]'>
                 Ủng hộ tôi
             </button>
         </div>
+        <br className='border-b-white'/>
     </div>
   )
 }
