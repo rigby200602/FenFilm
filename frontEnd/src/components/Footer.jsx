a7aef5e36a391eb08f267d1ee2c6b1bac07e0d68
rigby200602 <tungle3456@gmail.com>
2025-09-07 17:24:05 +0700

    edit

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-0 text-white h-[25%] w-screen px-[5%]">
+    <div className="absolute bottom-0 text-white h-[25%] w-full px-[5%]">
       <div className="bg-white h-[1px] my-4"></div>
-      <div className="">Footer</div>
+      <h1 className="text-end text-4xl font-bold cursor-pointer">FenFilm</h1>
     </div>
   );
 };
