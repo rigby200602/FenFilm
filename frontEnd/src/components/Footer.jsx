db18b38c3e78f341c49d90c21ca19f3f8dba6c16
rigby200602 <tungle3456@gmail.com>
2025-09-07 17:00:45 +0700

    done blueprint footer

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="absolute bottom-0 text-white h-[25%]">
-      <div className="bg-white h-[1px] my-4 min-w-screen"></div>
+    <div className="absolute bottom-0 text-white h-[25%] w-screen px-[5%]">
+      <div className="bg-white h-[1px] my-4"></div>
       <div className="">Footer</div>
     </div>
   );
