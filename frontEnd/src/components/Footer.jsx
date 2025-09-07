d720490c65b99b94acd675c60718492643d7e6d6
rigby200602 <tungle3456@gmail.com>
2025-09-07 16:52:42 +0700

    need to fix this

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='absolute bottom-0 text-white h-[20%] mx-[5%]'>
-        Footer
+    <div className="absolute bottom-0 text-white h-[25%]">
+      <div className="bg-white h-[1px] my-4 min-w-screen"></div>
+      <div className="">Footer</div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
