84bb3ca0562d3b955b992b080a8d0b65833a7439
rigby200602 <tungle3456@gmail.com>
2025-09-08 21:16:07 +0700

    fix h

--- frontEnd/src/components/Carousel.jsx
+++ frontEnd/src/components/Carousel.jsx
@@ -4,7 +4,7 @@ import DP from '../assets/DP.jpg'
 const Carousel = () => {
   return (
     <div className='w-full h-[30%] px-[5%] my-[2%]'>
-        <img src={DP} alt='Danny Phantom' className='w-full h-50'/>
+        <img src={DP} alt='Danny Phantom' className='w-full max-h-90'/>
     </div>
   )
 }
