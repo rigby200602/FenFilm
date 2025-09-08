240be8a26e3c73951cff4aaab8a6a3f2b0c6679e
rigby200602 <tungle3456@gmail.com>
2025-09-08 22:56:45 +0700

    set up state

--- frontEnd/src/components/Carousel.jsx
+++ frontEnd/src/components/Carousel.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DP from '../assets/DP.jpg'
 
 const Carousel = () => {
+  const [slide, setSlide] = useState(0)
   return (
     <div className='w-full px-[5%] my-[2%]'>
         <img src={DP} alt='Danny Phantom' className='w-full max-h-100'/>
