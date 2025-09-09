3fc0bd3828d9d5b22c6ce53d05f0ed1b158701dd
rigby200602 <tungle3456@gmail.com>
2025-09-10 00:50:53 +0700

    change size

--- frontEnd/src/components/Carousel.jsx
+++ frontEnd/src/components/Carousel.jsx
@@ -14,9 +14,9 @@ const Carousel = () => {
 
   return (
     <div className='w-full px-[5%] my-[2%]'>
-      <BsArrowLeftCircleFill/>
+      <BsArrowLeftCircleFill size={50}/>
         <img src={DP} alt='Danny Phantom' className='w-full max-h-100'/>
-      <BsArrowRightCircleFill/>
+      <BsArrowRightCircleFill size={50}/>
     </div>
   )
 }
