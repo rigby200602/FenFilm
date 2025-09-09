393dcd21ee6e92540bac29fe8665aa1a719cc8a0
rigby200602 <tungle3456@gmail.com>
2025-09-10 00:47:35 +0700

    add left, right arrow icon

--- frontEnd/src/components/Carousel.jsx
+++ frontEnd/src/components/Carousel.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import DP from '../assets/DP.jpg'
 import slides from '../data/slides.json'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 const Carousel = () => {
   const [slide, setSlide] = useState(0)
@@ -13,7 +14,9 @@ const Carousel = () => {
 
   return (
     <div className='w-full px-[5%] my-[2%]'>
+      <BsArrowLeftCircleFill/>
         <img src={DP} alt='Danny Phantom' className='w-full max-h-100'/>
+      <BsArrowRightCircleFill/>
     </div>
   )
 }
