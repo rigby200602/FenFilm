eb7080afb4f0a20179f94551f8ab9f5d0b5a9b09
rigby200602 <tungle3456@gmail.com>
2025-09-10 21:15:30 +0700

    add hover

--- frontEnd/src/components/Carousel.jsx
+++ frontEnd/src/components/Carousel.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react'
-import DP from '../assets/DP.jpg'
+import React, { useState } from "react";
+import DP from "../assets/DP.jpg";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
-const Carousel = ({data}) => {
-  const [slide, setSlide] = useState(0)
+const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
   const nextSlide = () => {
-    setSlide(slide === data.length - 1 ? 0 : slide + 1)
-  }
+    setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  };
   const prevSlide = () => {
-    setSlide(slide === data.length - 1 ? 0 : slide - 1)
-  }
+    setSlide(slide === data.length - 1 ? 0 : slide - 1);
+  };
 
   return (
-    <div className='flex relative justify-center items-center w-full px-[5%] my-[2%]'>
-      <BsArrowLeftCircleFill size={50} onClick={prevSlide} color='white'
-      className='absolute cursor-pointer left-0 ml-[5%]'/>
-        <img src={DP} alt='Danny Phantom' className='w-full max-h-120'/>
-      <BsArrowRightCircleFill size={50} onClick={nextSlide} color='white'
-      className='absolute cursor-pointer right-0 mr-[5%]'/>
-    </div>
-  )
-}
+    <div className="flex relative justify-center items-center w-full px-[5%] my-[2%]">
+      <span className="hover:opacity-50">
+        <BsArrowLeftCircleFill
+          size={50}
+          onClick={prevSlide}
+          color="white"
+          className="absolute cursor-pointer left-0 ml-[5%]"
+        />
+      </span>
 
-export default Carousel
+      <img src={DP} alt="Danny Phantom" className="w-full max-h-120" />
+      <span className="hover:opacity-50">
+        <BsArrowRightCircleFill
+          size={50}
+          onClick={nextSlide}
+          color="white"
+          className="absolute cursor-pointer right-0 mr-[5%]"
+        />
+      </span>
+    </div>
+  );
+};
+
+export default Carousel;
