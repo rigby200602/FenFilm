2d2231a5c007f54914d5ad0cf2fe320a5c881cf4
rigby200602 <tungle3456@gmail.com>
2025-09-10 21:20:22 +0700

    import slides to components

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,14 @@
 import Carousel from "./components/Carousel"
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
+import slides from "./data/slides"
 
 function App() {
 
   return (
     <div>
       <NavBar />
-      <Carousel />
+      <Carousel data={slides}/>
       <Footer />
     </div>
   )
