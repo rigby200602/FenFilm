c6b52c3255a9968d50eb16c1c9018aaaf1f22d7b
rigby200602 <tungle3456@gmail.com>
2025-09-12 20:20:57 +0700

    updata margin y

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ListCartoon = (props) => {
   return (
-    <div className='h-[15%] mx-[5%] my-4'>
+    <div className='h-[15%] mx-[5%] my-8'>
         <h1 className='text-3xl text-white'>{props.title}</h1>
     </div>
   )
