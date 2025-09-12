6eb1dbdec4b689efb58be23838c3beb49c3a5678
rigby200602 <tungle3456@gmail.com>
2025-09-12 20:20:31 +0700

    add component to app

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import Carousel from "./components/Carousel"
 import Footer from "./components/Footer"
+import ListCartoon from "./components/ListCartoon"
 import NavBar from "./components/NavBar"
 import {slides} from "./data/slides"
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <NavBar />
       <Carousel data={slides}/>
+      <ListCartoon title={"Phim mới cập nhật"}/>
       <Footer />
     </div>
   )
