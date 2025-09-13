2d6392f9250e03702a5fb44aee7f7b6012c86ae1
rigby200602 <tungle3456@gmail.com>
2025-09-13 11:30:00 +0700

    pointer, hover scale

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Box = ({data}) => {
   return (
-    <div className="text-white my-8 w-30 md:w-60">
+    <div className="text-white my-8 w-30 md:w-70 cursor-pointer transition hover:scale-105">
       {data.map((item, i) => {
         return <img key={i} src={item.thumnail} />;
       })}
