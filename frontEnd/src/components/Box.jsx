7ba1ea1ccb9a680d8501fd4c9418101e8569400a
rigby200602 <tungle3456@gmail.com>
2025-09-13 10:43:49 +0700

    new data to render thumnail

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = (props) => {
   return (
     <div className='max-h-[150px] text-white my-8'>
-        <img src={props.src}/>
+        <img src={props.thumnail}/>
     </div>
   )
 }
