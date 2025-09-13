637b0290d172f263b1907616105d8001aed83c19
rigby200602 <tungle3456@gmail.com>
2025-09-13 09:21:52 +0700

    add box components

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -4,6 +4,7 @@ const ListCartoon = (props) => {
   return (
     <div className='h-[15%] mx-[5%] my-8'>
         <h1 className='text-3xl text-white'>{props.title}</h1>
+        {/* Show box */}
     </div>
   )
 }
