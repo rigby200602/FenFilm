3e24b11d7a789014e31985fe798ce1e096457f26
rigby200602 <tungle3456@gmail.com>
2025-09-13 09:43:46 +0700

    update css

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,3 +1,4 @@
+import Box from "./components/Box"
 import Carousel from "./components/Carousel"
 import Footer from "./components/Footer"
 import ListCartoon from "./components/ListCartoon"
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Carousel data={slides}/>
       <ListCartoon title={"Phim mới cập nhật"}/>
+      <Box/>
       <Footer />
     </div>
   )
--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Box = (props) => {
   return (
-    <div className='max-h-[150px]'>
+    <div className='max-h-[150px] text-white mx-[5%] my-8'>
         box
     </div>
   )
