e838dcbb03c1b6a6638785e02c5b62eb30e3ec25
rigby200602 <tungle3456@gmail.com>
2025-09-14 17:09:49 +0700

    done box

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Box = () => {
+const Box = ( {data} ) => {
   return (
-    <div>Box</div>
+    <div className="text-white my-8 w-30 md:w-70 cursor-pointer transition hover:scale-105">
+              <img src={data.thumnail} />
+              <h1 className="text-start my-2">{data.title}</h1>
+    </div>
   )
 }
 
--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Box from "./Box";
 
 const ListCartoon = (props) => {
   return (
@@ -6,12 +7,9 @@ const ListCartoon = (props) => {
       <h1 className="text-3xl text-white">{props.title}</h1>
       {/* Show box */}
       <div className="flex gap-4">
-        {props.data.map((item, i) => {
+        {props.data.map((item,i) => {
           return (
-            <div className="text-white my-8 w-30 md:w-70 cursor-pointer transition hover:scale-105">
-              <img key={i} src={item.thumnail} />
-              <h1 className="text-start my-2">{item.title}</h1>
-            </div>
+              <Box key={i} data={item} />
           );
         })}
       </div>
