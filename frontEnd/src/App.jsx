6f3e4f7b3f26f03b2c90bf28e25f2b2b5d6b7dbf
rigby200602 <tungle3456@gmail.com>
2025-09-14 17:03:39 +0700

    render box without components

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer"
 import ListCartoon from "./components/ListCartoon"
 import NavBar from "./components/NavBar"
 import {slides} from "./data/slides.json"
+import { dannyPhantom } from "./data/list.json"
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <div>
       <NavBar />
       <Carousel data={slides}/>
-      <ListCartoon title={"Phim mới cập nhật"}/>
+      <ListCartoon title={"Phim mới cập nhật"} data={dannyPhantom}/>
       <Footer />
     </div>
   )
--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Box from './Box'
-import { dannyPhantom } from '../data/list.json'
+import React from "react";
 
 const ListCartoon = (props) => {
   return (
-    <div className='h-[15%] mx-[5%] my-8'>
-        <h1 className='text-3xl text-white'>{props.title}</h1>
-        {/* Show box */}
-        <div className='grid grid-cols-4 gap-4'>
-            <Box data={dannyPhantom}/>
-        </div>
+    <div className="h-[15%] mx-[5%] my-8">
+      <h1 className="text-3xl text-white">{props.title}</h1>
+      {/* Show box */}
+      <div className="flex gap-4">
+        {props.data.map((item, i) => {
+          return (
+            <div className="text-white my-8 w-30 md:w-70 cursor-pointer transition hover:scale-105">
+              <img key={i} src={item.thumnail} />
+              <h1 className="text-start my-2">{item.title}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListCartoon
+export default ListCartoon;
