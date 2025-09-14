e2a5b9f91e61e7ea65607b23535d7915e1d4c6ae
rigby200602 <tungle3456@gmail.com>
2025-09-14 17:10:23 +0700

    fix content button

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
         <h1 className="text-4xl font-bold cursor-pointer">FenFilm</h1>
         <div className="ml-auto">
           <button className="rounded-4xl cursor-pointer px-4 py-2 bg-white text-[#17252A] hover:bg-purple-800 hover:text-white">
-            Ủng hộ tôi
+            Chúng tôi
           </button>
         </div>
       </div>
