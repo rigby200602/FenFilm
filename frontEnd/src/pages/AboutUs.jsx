6f83d3911efa5fdb5650477760291e26de36edc4
rigby200602 <tungle3456@gmail.com>
2025-09-15 20:25:31 +0700

    spell error 2

--- frontEnd/src/pages/AboutUs.jsx
+++ frontEnd/src/pages/AboutUs.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
         <div className='text-white mx-[5%] text-2xl leading-10'>
             <h1 className='text-4xl text-center'>Chúng tôi là ai?</h1>
             <p>Xin chào các bạn!</p>
-            <p>Mình là Fensub, ngươi đang thực hiện các dự ác dịch thuật các phim hoạt hình cũ trên các kênh TV ngày ấy.</p>
+            <p>Mình là Fensub, người đang thực hiện các dự ác dịch thuật các phim hoạt hình cũ trên các kênh TV ngày ấy.</p>
             <p>Và đây hiện tại là dự án tiếp theo ccuar mình muốn thực hiện, nhằm tạo ra một trang web nhầm tổng hợp những tập phim mình đã làm,
                 để mọi người có thể dễ dàng theo dõi và có nền tảng tiện để xem phim hơn.
             </p>
