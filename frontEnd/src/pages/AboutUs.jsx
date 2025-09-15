b8e1f641d01693c094c6adadd7277feb3c19e3eb
rigby200602 <tungle3456@gmail.com>
2025-09-15 20:15:39 +0700

    update css

--- frontEnd/src/pages/AboutUs.jsx
+++ frontEnd/src/pages/AboutUs.jsx
@@ -6,9 +6,11 @@ const AboutUs = () => {
   return (
     <div className='text-white'>
         <NavBar/>
-        <div className='mx-[5%] text-2xl'>
+        <div className='mx-[5%] text-2xl leading-10'>
             <h1 className='text-4xl text-center'>Chúng tôi là ai?</h1>
-            <h2>Xin chào các bạn!</h2>
+            <p>Xin chào các bạn!</p>
+            <p>Mình là Fensub, ngươi đang thực hiện các dự ác dịch thuật các phim hoạt hình cũ trên các kênh TV ngày ấy.</p>
+            <p></p>
         </div>
         <Footer/>
     </div>
