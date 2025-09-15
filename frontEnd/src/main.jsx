06bfa1588c9a13d64118261d4f1f2dc4620bd6de
rigby200602 <tungle3456@gmail.com>
2025-09-15 20:00:09 +0700

    routing

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import AboutUs from './pages/AboutUs.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path='/about-us' element={<AboutUs/>} />
     </Routes>
   </BrowserRouter>,
 )
