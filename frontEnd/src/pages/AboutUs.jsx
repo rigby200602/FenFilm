c8f9e5910b47a8fb72ab8f75e53855df08f430fb
rigby200602 <tungle3456@gmail.com>
2025-09-15 20:22:23 +0700

    spelling error

--- frontEnd/src/pages/AboutUs.jsx
+++ frontEnd/src/pages/AboutUs.jsx
@@ -4,16 +4,16 @@ import NavBar from '../components/NavBar'
 
 const AboutUs = () => {
   return (
-    <div className='text-white'>
+    <div>
         <NavBar/>
-        <div className='mx-[5%] text-2xl leading-10'>
+        <div className='text-white mx-[5%] text-2xl leading-10'>
             <h1 className='text-4xl text-center'>Chúng tôi là ai?</h1>
             <p>Xin chào các bạn!</p>
             <p>Mình là Fensub, ngươi đang thực hiện các dự ác dịch thuật các phim hoạt hình cũ trên các kênh TV ngày ấy.</p>
             <p>Và đây hiện tại là dự án tiếp theo ccuar mình muốn thực hiện, nhằm tạo ra một trang web nhầm tổng hợp những tập phim mình đã làm,
                 để mọi người có thể dễ dàng theo dõi và có nền tảng tiện để xem phim hơn.
             </p>
-            <p>Một lần nữa, mình muốn gửi lời ccamr ơn đến tất cả các bạn đã luôn ủng hộ mình và sẽ tiếp tục đồng hàng cùng mình trong những dự ác sắp tới.</p>
+            <p>Một lần nữa, mình muốn gửi lời cảm ơn đến tất cả các bạn đã luôn ủng hộ mình và sẽ tiếp tục đồng hành cùng mình trong những dự án sắp tới.</p>
         </div>
         <Footer/>
     </div>
