48f1f74e6f90e5a50893b55fcb376da60cbdd136
rigby200602 <tungle3456@gmail.com>
2025-09-15 20:19:24 +0700

    update content

--- frontEnd/src/pages/AboutUs.jsx
+++ frontEnd/src/pages/AboutUs.jsx
@@ -10,7 +10,10 @@ const AboutUs = () => {
             <h1 className='text-4xl text-center'>Chúng tôi là ai?</h1>
             <p>Xin chào các bạn!</p>
             <p>Mình là Fensub, ngươi đang thực hiện các dự ác dịch thuật các phim hoạt hình cũ trên các kênh TV ngày ấy.</p>
-            <p></p>
+            <p>Và đây hiện tại là dự án tiếp theo ccuar mình muốn thực hiện, nhằm tạo ra một trang web nhầm tổng hợp những tập phim mình đã làm,
+                để mọi người có thể dễ dàng theo dõi và có nền tảng tiện để xem phim hơn.
+            </p>
+            <p>Một lần nữa, mình muốn gửi lời ccamr ơn đến tất cả các bạn đã luôn ủng hộ mình và sẽ tiếp tục đồng hàng cùng mình trong những dự ác sắp tới.</p>
         </div>
         <Footer/>
     </div>
