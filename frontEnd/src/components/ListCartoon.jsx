70613015cb589eaa10e1a9233a399722aa832349
rigby200602 <tungle3456@gmail.com>
2025-09-16 23:00:11 +0700

    update reponsive

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -6,7 +6,7 @@ const ListCartoon = (props) => {
     <div className="h-[15%] mx-[5%] my-8">
       <h1 className="text-3xl text-white">{props.title}</h1>
       {/* Show box */}
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:gird-col-5 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:gird-col-4 gap-4">
         {props.data.map((item,i) => {
           return (
               <Box key={i} data={item} />
