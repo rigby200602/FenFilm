b70701dd7a94a7fa562b405c2024b8a6e66114ec
rigby200602 <tungle3456@gmail.com>
2025-09-16 22:52:29 +0700

    fix responsive

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="w-full mb-[2%]">
-      <div className="flex text-white mx-[5%] mt-4">
+      <div className="flex text-white mx-[5%] mt-4 justify-center">
         <Link to={"/"} className="text-4xl font-bold cursor-pointer">FenFilm</Link>
-        <div className="ml-auto">
-          <Link to={"/about-us"} className="hidden md:block rounded-4xl cursor-pointer px-4 py-2 bg-white text-[#17252A] hover:bg-purple-800 hover:text-white">
+        <div className="ml-auto hidden md:block">
+          <Link to={"/about-us"} className="rounded-4xl cursor-pointer px-4 py-2 bg-white text-[#17252A] hover:bg-purple-800 hover:text-white">
             Chúng tôi
           </Link>
         </div>
