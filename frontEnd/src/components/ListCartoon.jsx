365b011b8ad7c321738d1f41eba1b5dc3bf565db
rigby200602 <tungle3456@gmail.com>
2025-09-17 21:49:50 +0700

    fix grid

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -3,10 +3,10 @@ import Box from "./Box";
 
 const ListCartoon = (props) => {
   return (
-    <div className="h-[15%] mx-[5%] my-8">
+    <div className="h-[15%] min-h- mx-[5%] my-8">
       <h1 className="text-3xl text-white">{props.title}</h1>
       {/* Show box */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:gird-col-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {props.data.map((item,i) => {
           return (
               <Box key={i} data={item} />
