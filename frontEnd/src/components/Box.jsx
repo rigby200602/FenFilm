9911d8b2a596feff5d2bdd7418d322351189b425
rigby200602 <tungle3456@gmail.com>
2025-09-17 21:51:24 +0700

    fix reponsive width

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Box = ( {data} ) => {
   return (
-    <div className="text-white my-8 w-30 md:w-70 cursor-pointer transition hover:scale-105">
+    <div className="text-white my-8 w-65 md:w-70 lg:w-80 cursor-pointer transition hover:scale-105">
               <img src={data.thumnail} />
               <h1 className="text-start my-2">{data.title}</h1>
     </div>
