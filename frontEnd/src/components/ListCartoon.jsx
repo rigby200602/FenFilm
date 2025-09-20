2fbd926414bb39426572f06b25ab4cbc3978b2db
rigby200602 <tungle3456@gmail.com>
2025-09-20 23:19:43 +0700

    fix collections path

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 const ListCartoon = (props) => {
   return (
     <div className="h-[15%] min-h- mx-[5%] my-8">
-      <h1 className="text-3xl text-white text-center md:text-start">{props.title}</h1>
+      <Link to={`/collections/${props.data.collection}`}>
+        <h1 className="text-3xl text-white text-center md:text-start">{props.title}</h1>
+      </Link>  
       {/* Show box */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {props.data.map((item,i) => {
-          return (
-              <Link to={`/collections/${item.collection}`}>
+          return (        
                 <Box key={i} data={item} />
-              </Link> 
           );
         })}
       </div>
