4c91e12928c1e6ee5f8e3c44dd3117eb5a9d3548
rigby200602 <tungle3456@gmail.com>
2025-09-22 22:27:58 +0700

    update collection

--- frontEnd/src/pages/Collections.jsx
+++ frontEnd/src/pages/Collections.jsx
@@ -3,14 +3,15 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
 
-const Collections = ({collections}) => {
+const Collections = ({data}) => {
   // get collection thorough id
   const {id} = useParams();
   return (
     <div className="mx-[5%] text-white">
       <NavBar />
       <div className="flex">
-        <h2>{collections.name}</h2>
+        <h2>{data.name}</h2>
+        <p>{data.description}</p>
       </div>
       <Footer />
     </div>
