79da4b130977ff69a7c2ddc492089d83c34abc73
rigby200602 <tungle3456@gmail.com>
2025-09-22 22:22:29 +0700

    buiild collection

--- frontEnd/src/pages/Collections.jsx
+++ frontEnd/src/pages/Collections.jsx
@@ -3,14 +3,14 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
 
-const Collections = () => {
+const Collections = ({collections}) => {
   // get collection thorough id
   const {id} = useParams();
   return (
     <div className="mx-[5%] text-white">
       <NavBar />
       <div className="flex">
-
+        <h2>{collections.name}</h2>
       </div>
       <Footer />
     </div>
