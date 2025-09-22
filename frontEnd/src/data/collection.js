e5a0f5b41d7bb97ac982ada4d8eb76b7bf67a762
rigby200602 <tungle3456@gmail.com>
2025-09-22 22:27:45 +0700

    update test data

--- frontEnd/src/data/collection.js
+++ frontEnd/src/data/collection.js
@@ -3,6 +3,6 @@
         "id": 1,
         "name": "Danny Phantom",
         "src": "/collection.jpg",
-        "description": ""
+        "description": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     }
 ]
