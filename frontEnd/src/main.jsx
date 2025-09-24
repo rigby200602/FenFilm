12e1066bb9a2d766fa0cdf0a610f1f3c3b64b874
rigby200602 <tungle3456@gmail.com>
2025-09-24 21:59:39 +0700

    update path

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import collection from "./data/collection.json"
+import collection from "./data/collections.json"
 import App from "./App.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Collections from "./pages/Collections.jsx";
--- frontEnd/src/pages/Collections.jsx
+++ frontEnd/src/pages/Collections.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
-import collections from '../data/collection.json'
-import list from '../data/list.json'
+import collections from '../data/collections.json'
+import lists from '../data/lists.json'
 
 const Collections = ({data}) => {
   // get collection thorough id
@@ -11,7 +11,7 @@ const Collections = ({data}) => {
   // create collection that have all film with same id
   const collection = collections.find(c => c.id === Number(id))
   // find film with same collection
-  const filmsInCollection = list.filter(f => f.collection === Number(id));
+  const filmsInCollection = lists.filter(f => f.collection === Number(id));
   // check if collection valid or not?
   if (!collection) return <div>Collection not found</div>;
   return (
