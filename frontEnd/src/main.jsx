d4612d21e5e6944e71587fb3bb942a3dfbe51b92
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:39:04 +0700

    update route in main

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")).render(
       <Route path="/" element={<App />} />
       <Route path="/about-us" element={<AboutUs />} />
       <Route path="/collections/:id" element={<Collections/>} />
+      <Route path="/play/:id" element={<Play/>} />
     </Routes>
   </BrowserRouter>
 );
