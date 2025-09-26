614ed62672672127f7dd4b2f6f37fcf2f6986c93
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:38:35 +0700

    update route

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Box = ( {data} ) => {
   return (
-    <div className="text-white my-8 max-w-65 md:max-w-70 lg:max-w-80 cursor-pointer transition hover:scale-105">
+    <Link to={'/play/:id'}
+    className="text-white my-8 max-w-65 md:max-w-70 lg:max-w-80 cursor-pointer transition hover:scale-105">
               <img src={data.thumnail} />
               <h1 className="text-start my-2">{data.title}</h1>
-    </div>
+    </Link>
   )
 }
 
