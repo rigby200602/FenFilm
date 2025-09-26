59b776f533bfdb7e8e1abeb76f4a5ed11eaaef0c
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:41:41 +0700

    import play page

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Collections from "./pages/Collections.jsx";
+import Play from "./pages/Play.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
