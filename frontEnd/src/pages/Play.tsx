16e95a1d336ae518e4965672aecc84ea5d4bb7be
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:40:31 +0700

    add cmt

--- frontEnd/src/pages/Play.tsx
+++ frontEnd/src/pages/Play.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const Play = () => {
+    // get id from url
     const {id} = useParams()
     return (
     <div>Play</div>
