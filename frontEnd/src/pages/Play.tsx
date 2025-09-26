b093ebedfd29a34783d06ce8e27fda9142ef433b
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:41:08 +0700

    add layout page

--- frontEnd/src/pages/Play.tsx
+++ frontEnd/src/pages/Play.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 const Play = () => {
     // get id from url
     const {id} = useParams()
     return (
-    <div>Play</div>
+    <div>
+        <NavBar />
+        <Footer />
+    </div>
   )
 }
 
