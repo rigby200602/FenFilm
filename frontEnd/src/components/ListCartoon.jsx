ba1126963aaf1ce454ed3a8f230996343d143b15
rigby200602 <tungle3456@gmail.com>
2025-09-26 23:35:32 +0700

    only show 4 box

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -3,14 +3,16 @@ import Box from "./Box";
 import { Link } from "react-router-dom";
 
 const ListCartoon = (props) => {
+  // only show 4 box
+  const data = props.data.filter(item => item.id < 5);
   return (
     <div className="h-[15%] min-h- mx-[5%] my-8">
       <Link to={`/collections/${props.data[0].collection}`}>
         <h1 className="text-3xl text-white text-center md:text-start">{props.title}</h1>
       </Link>  
       {/* Show box */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {props.data.map((item,i) => {
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-auto">
+        {data.map((item,i) => {
           return (        
                 <Box key={i} data={item} />
           );
