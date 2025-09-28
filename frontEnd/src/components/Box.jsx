d59e42fe591efdb511da5be330e1ad9cb74432ed
rigby200602 <tungle3456@gmail.com>
2025-09-28 15:29:21 +0700

    update dynamic path

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Box = ( {data} ) => {
   return (
-    <Link to={'/play/:id'}
+    <Link to={`/play/${data.id}`}
     className="text-white my-8 max-w-65 md:max-w-70 lg:max-w-80 cursor-pointer transition hover:scale-105">
               <img src={data.thumnail} />
               <h1 className="text-start my-2">{data.title}</h1>
