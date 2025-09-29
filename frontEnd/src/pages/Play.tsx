571e9030345952b301104bb42babd822cb7a5831
rigby200602 <tungle3456@gmail.com>
2025-09-29 22:04:49 +0700

    allow pip and fullscreen

--- frontEnd/src/pages/Play.tsx
+++ frontEnd/src/pages/Play.tsx
@@ -20,7 +20,7 @@ const Play = () => {
             key={id}
             src={filmUrl}
             title={film?.title}
-            allow='autoplay'
+            allow='autoplay; fullscreen; picture-in-picture'
             className="w-full h-[80vh] rounded-lg bg-black"
           >
           </iframe>
