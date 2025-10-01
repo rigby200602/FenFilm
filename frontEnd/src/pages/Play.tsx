dd5572fa4695d8457cbd97962defb0355c845111
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:31:11 +0700

    import season

--- frontEnd/src/pages/Play.tsx
+++ frontEnd/src/pages/Play.tsx
@@ -14,7 +14,6 @@ const Play = () => {
     const film = lists.find(film => film.id === Number(id))
     // get film url
     const filmUrl = film ? film.url : ''
-    console.log(filmUrl)
     return (
     <div>
         <NavBar />
