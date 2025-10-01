4c98ba0c973485d94a54df31591aa06c0f97353a
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:46:03 +0700

    update get film data

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -5,11 +5,11 @@ import { useParams } from 'react-router-dom'
 const Season = () => {
   // fetch id from url
   const {id} = useParams();
-  // fetch season data
-  const season = lists.filter(s => s.season === Number(id));
+  // get film data from id
+  const filmData = lists.find(s => s.id === Number(id));
   return (
     <div className='flex mx-[5%] my-4 text-white'>
-        <h1 className='text-3xl hidden md:block'>{season.name}</h1>
+        {/* <h1 className='text-3xl hidden md:block'>{season.name}</h1> */}
         {}
     </div>
   )
