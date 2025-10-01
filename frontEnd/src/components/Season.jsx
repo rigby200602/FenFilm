d16d6642f56952b10036557ef9d451ad9224207f
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:32:06 +0700

    fix prop value

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -9,7 +9,7 @@ const Season = () => {
   const season = lists.filter(s => s.season === Number(id));
   return (
     <div className='flex mx-[5%] my-4 text-white'>
-        <h1 className='text-3xl hidden md:block'>{data.name}</h1>
+        <h1 className='text-3xl hidden md:block'>{season.name}</h1>
         {}
     </div>
   )
