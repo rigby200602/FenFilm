499c16780cb2684f71ebd67ab6ff6d3281910333
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:21:05 +0700

    fetch season data

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import lists  from '../data/lists.json'
 
 const Season = () => {
+  // fetch season data
+  const season = lists.filter(film => film.season === data.id);
   return (
     <div className='flex mx-[5%] my-4 text-white'>
         <h1 className='text-3xl hidden md:block'>{data.name}</h1>
