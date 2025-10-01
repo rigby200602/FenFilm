e36c1f0c0622bfb85d6a539962a4273dc99a24de
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:18:35 +0700

    delete prop

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Season = ({data}) => {
+const Season = () => {
   return (
     <div className='flex mx-[5%] my-4 text-white'>
         <h1 className='text-3xl hidden md:block'>{data.name}</h1>
+        {}
     </div>
   )
 }
