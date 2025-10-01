8042aa038077165d263ca4b567344f27319e5fed
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:22:31 +0700

    update fetch

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -3,7 +3,7 @@ import lists  from '../data/lists.json'
 
 const Season = () => {
   // fetch season data
-  const season = lists.filter(film => film.season === data.id);
+  const season = lists.filter(s => s.season === Number(id));
   return (
     <div className='flex mx-[5%] my-4 text-white'>
         <h1 className='text-3xl hidden md:block'>{data.name}</h1>
