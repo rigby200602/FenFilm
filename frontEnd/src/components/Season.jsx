59122cc061835fe479b6939aa3e69d9b584ecb41
rigby200602 <tungle3456@gmail.com>
2025-10-01 22:16:26 +0700

    reponsive h1

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Season = ({data}) => {
   return (
     <div className='flex mx-[5%] my-4 text-white'>
-        <h1 className='text-3xl'>{data.name}</h1>
+        <h1 className='text-3xl hidden md:block'>{data.name}</h1>
     </div>
   )
 }
