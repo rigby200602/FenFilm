060c4554da77aa74c33362eee8ffcd964ea6f30d
rigby200602 <tungle3456@gmail.com>
2025-10-02 22:36:25 +0700

    context provider

--- frontEnd/src/context/AppContext.jsx
+++ frontEnd/src/context/AppContext.jsx
@@ -1,3 +1,11 @@
 import React, { createContext } from 'react'
 
 export default AppContext = createContext()
+
+export const AppProvider = ({children}) => {
+  return (
+    <AppContext.Provider value={{}}>
+      {children}
+    </AppContext.Provider>
+  )
+}
