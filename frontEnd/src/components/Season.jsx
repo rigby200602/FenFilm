0c8adf0c046791857b3280972712dcb21e8fe430
rigby200602 <tungle3456@gmail.com>
2025-10-02 22:48:49 +0700

    call filmData from id

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import lists  from '../data/lists.json'
 import { useParams } from 'react-router-dom'
+import { filmData } from '../context/AppContext'
 
 const Season = () => {
   // fetch id from url
   const {id} = useParams();
   // get film data from id
-  const filmData = lists.find(s => s.id === Number(id));
+  const film = filmData(id);
   return (
     <div className='flex mx-[5%] my-4 text-white'>
         {/* <h1 className='text-3xl hidden md:block'>{season.name}</h1> */}
