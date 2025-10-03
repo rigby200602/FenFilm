74bbd0e067bb809098b60d063918c3afc4d41e4a
rigby200602 <tungle3456@gmail.com>
2025-10-03 21:25:06 +0700

    import data, find season though data

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import lists  from '../data/lists.json'
 import { useParams } from 'react-router-dom'
 import { filmData } from '../context/AppContext'
+import seasons from '../data/seasons.json'
 
-const Season = () => {
+const Season = ({data}) => {
   // fetch id from url
   const {id} = useParams();
+  // get season data
+  const season = data;
   // get film data from id
   const film = filmData(id);
   return (
     <div className='flex mx-[5%] my-4 text-white'>
-        {/* <h1 className='text-3xl hidden md:block'>{season.name}</h1> */}
+        <h1 className='text-3xl hidden md:block'>{data.name}</h1>
         {}
     </div>
   )
