1988c18f89c8f30914799e05a2401de6e7fc6e81
rigby200602 <tungle3456@gmail.com>
2025-10-05 01:13:28 +0700

    update reponsive

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
-import lists  from '../data/lists.json'
-import { useParams } from 'react-router-dom'
-import { filmData } from '../context/AppContext'
-import seasons from '../data/seasons.json'
+import React from "react";
+import lists from "../data/lists.json";
+import { useParams } from "react-router-dom";
+import { filmData } from "../context/AppContext";
+import seasons from "../data/seasons.json";
 
-const Season = ({data}) => {
+const Season = ({ data }) => {
   // fetch id from url
-  const {id} = useParams();
+  const { id } = useParams();
   // get season data
-  const season = seasons.find(s => s.id === data)
+  const season = seasons.find((s) => s.id === data);
   // get list film through season
-  const listFilm = lists.filter(l => l.season === season.id)
+  const listFilm = lists.filter((l) => l.season === season.id);
   console.log(listFilm);
   return (
-    <div className='lg:flex mx-[5%] mb-4 text-white grid grid-cols-4 md:grid-cols-6 gap-4'>
-        <h1 className='text-xl hidden lg:block w-70'>{season.name}</h1>
-          {listFilm.map((item,id) => (
-          <div key={id} className='mx-2 w-18 md:w-20 h-8 cursor-pointer py-0.5
+    <div className="mx-[5%] mb-4 text-white flex">
+      <h1 className="text-xl hidden lg:block w-70">{season.name} :</h1>
+      <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 gap-2">
+        {listFilm.map((item, id) => (
+          <div
+            key={id}
+            className="w-24 h-10 cursor-pointer py-[5%] mx-auto
           hover:border hover:border-white hover:rounded-2xl hover:text-black hover:bg-white
           // for reponsive
           border border-white rounded-2xl text-black bg-white
           lg:border-0 lg:rounded-none lg:text-white lg:bg-transparent
-          '
+          "
           >
-            <p className='text-center'>{item.id}</p>
+            <p className="text-center">{item.id}</p>
           </div>
         ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Season
+export default Season;
