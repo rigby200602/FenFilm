bd99ce8a1da8c309d894feda3cdf7ee68fc802df
rigby200602 <tungle3456@gmail.com>
2025-10-07 20:58:23 +0700

    update context provider

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -5,14 +5,17 @@ import App from "./App.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Collections from "./pages/Collections.jsx";
 import Play from "./pages/Play.jsx";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about-us" element={<AboutUs />} />
-      <Route path="/collections/:id" element={<Collections/>} />
-      <Route path="/play/:id" element={<Play/>} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+    <AppContextProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/collections/:id" element={<Collections />} />
+        <Route path="/play/:id" element={<Play />} />
+      </Routes>
+    </AppContextProvider>
+    </BrowserRouter>
 );
--- frontEnd/src/pages/Play.tsx
+++ frontEnd/src/pages/Play.tsx
@@ -1,45 +1,49 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
-import Season from '../components/Season'
-import { filmData } from '../context/AppContext'
-import collections from '../data/collections.json'
-
-
+import React, {  useContext } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Season from "../components/Season";
+import collections from "../data/collections.json";
+import { AppContext } from "../context/AppContext";
 
 const Play = () => {
-    // get id from url
-    const {id} = useParams()
-    // fetch film url from database
-    const film = filmData(id || '')
-    // get film url
-    const filmUrl = film ? film.url : ''
-    // get id collection
-    const collectionId = film ? film.collection : null
-    // get seasons from collection
-    const seasons = collectionId ? collections.find(c => c.id === collectionId)?.seasons : []
-    return (
-    <div>
-        <NavBar />
-        <div className="my-5 mx-[5%]">
-          <iframe
-            key={id}
-            src={filmUrl}
-            title={film?.title}
-            loading='lazy'
-            allow='autoplay; picture-in-picture; fullscreen'
-            className="w-full h-[60vh] md:h-[80vh] rounded-lg bg-black"
-          >
-          </iframe>
-        </div>
-        {/* Check if seasons null or not, will render if have any attribute */}
-        { seasons ? (seasons.map((season,id) => {
-          return <Season key={id} data={season}/>
-        })) : null }
-        <Footer />
-    </div>
-  )
-}
+  // call context
+  const { filmData } = useContext(AppContext);
 
-export default Play
+  // get id from url
+  const { id } = useParams();
+  // fetch film url from database
+  const film = filmData(id || "");
+  // get film url
+  const filmUrl = film ? film.url : "";
+  // get id collection
+  const collectionId = film ? film.collection : null;
+  // get seasons from collection
+  const seasons = collectionId
+    ? collections.find((c) => c.id === collectionId)?.seasons
+    : [];
+  return (
+    <div>
+      <NavBar />
+      <div className="my-5 mx-[5%]">
+        <iframe
+          key={id}
+          src={filmUrl}
+          title={film?.title}
+          loading="lazy"
+          allow="autoplay; picture-in-picture; fullscreen"
+          className="w-full h-[60vh] md:h-[80vh] rounded-lg bg-black"
+        ></iframe>
+      </div>
+      {/* Check if seasons null or not, will render if have any attribute */}
+      {seasons
+        ? seasons.map((season, id) => {
+            return <Season key={id} data={season} />;
+          })
+        : null}
+      <Footer />
+    </div>
+  );
+};
+
+export default Play;
