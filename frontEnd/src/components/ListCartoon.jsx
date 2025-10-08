7c4d68f90a5c359a5e1f17f7c697062efd15e2b0
rigby200602 <tungle3456@gmail.com>
2025-10-08 22:41:11 +0700

    conditional rendering

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 
 const ListCartoon = (props) => {
   // only show 4 box
-  const data = props.data.filter(item => item.id < 5);
+  const data = props.data.filter((item) => item.id < 5);
   return (
     <div className="h-[15%] min-h- mx-[5%] mt-8">
       <Link to={`/collections/${props.collection}`}>
-        <h1 className="text-3xl text-white text-center md:text-start">{props.title}</h1>
-      </Link>  
+        <h1 className="text-3xl text-white text-center md:text-start">
+          {props.title}
+        </h1>
+      </Link>
       {/* Show box */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data.map((item,i) => {
-          return (        
-                <Box key={i} data={item} />
-          );
-        })}
-      </div>
+      {/* Check if data have any attribute, only render if have any */}
+      {data.length > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {data.map((item, i) => {
+            return <Box key={i} data={item} />;
+          })}
+        </div>
+      ) : null}
     </div>
   );
 };
