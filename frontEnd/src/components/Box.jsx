f59562c1d19fe1c14b7890e62a5c1a7a661ada27
rigby200602 <tungle3456@gmail.com>
2025-10-09 22:28:02 +0700

    update reponsive

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Box = ( {data} ) => {
   return (
     <Link to={`/play/${data.id}`}
-    className="text-white mt-4 max-w-65 md:max-w-70 lg:max-w-80 cursor-pointer transition hover:scale-105">
+    className="text-white mt-4 max-w-75 md:max-w-78 lg:max-w-80 cursor-pointer transition hover:scale-105">
               <img src={data.thumnail}
               loading='lazy' />
               <h1 className="text-start my-2">{data.title}</h1>
