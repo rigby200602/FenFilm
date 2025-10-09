ef514392fb84b0182df9461760d9fd38c9db0ceb
rigby200602 <tungle3456@gmail.com>
2025-10-09 22:04:25 +0700

    fix render condition

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -12,6 +12,8 @@ const ListCartoon = (props) => {
   console.log(list)
   return (
     <div className="h-[15%] min-h- mx-[5%] mt-8">
+      {data.length > 0 ? (
+      <>
       <Link to={`/collections/${props.collection}`}>
         <h1 className="text-3xl text-white text-center md:text-start">
           {props.title}
@@ -19,13 +21,13 @@ const ListCartoon = (props) => {
       </Link>
       {/* Show box */}
       {/* Check if data have any attribute, only render if have any */}
-      {data.length > 0 ? (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {data.map((item, i) => {
             return <Box key={i} data={item} />;
           })}
         </div>
-      ) : null}
+      </>
+      ) : null }
     </div>
   );
 };
