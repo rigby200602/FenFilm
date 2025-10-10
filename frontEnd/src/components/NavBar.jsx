18f860c49328ee3cf9bf4ac4b4af29a4e3a1ac51
rigby200602 <tungle3456@gmail.com>
2025-10-10 20:32:45 +0700

    update menu for small screen

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { RiMenu3Line } from "react-icons/ri";
 
 const NavBar = () => {
   return (
     <div className="w-full mb-[2%]">
       <div className="flex text-white mx-[5%] mt-4 justify-center">
-        <Link to={"/"} className="text-4xl font-bold cursor-pointer">FenFilm</Link>
+        <Link to={"/"} className="text-4xl font-bold cursor-pointer">
+          FenFilm
+        </Link>
         <div className="ml-auto hidden md:block">
-          <Link to={"/about-us"} className="rounded-4xl cursor-pointer px-4 py-2 bg-white text-[#17252A] hover:bg-purple-800 hover:text-white">
+          <Link
+            to={"/about-us"}
+            className="rounded-4xl cursor-pointer px-4 py-2 bg-white text-[#17252A] hover:bg-purple-800 hover:text-white"
+          >
             Chúng tôi
           </Link>
+        </div>
+        {/* Menu for small screen */}
+        <div className="block md:hidden ml-auto">
+          <RiMenu3Line
+            size={40}
+            className="text-white ml-[5%] mt-2 cursor-pointer"
+          />
         </div>
       </div>
       {/* White line */}
