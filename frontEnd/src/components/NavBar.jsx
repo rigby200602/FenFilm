525fdb7944a6e6afc61c940793a49ef41059372d
rigby200602 <tungle3456@gmail.com>
2025-10-11 17:11:13 +0700

    update

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -10,10 +10,22 @@ const NavBar = () => {
   return (
     <div className="w-full mb-[2%]">
       <div className="flex text-white mx-[5%] mt-4 justify-center">
-        <Link
-          to={"/"}
-          className="text-4xl font-bold cursor-pointer text-center"
-        >
+        {/* Menu for small screen */}
+        <div className="absolute ml-[5%] left-0 md:hidden">
+          <RiMenu3Line
+            size={40}
+            className="text-white cursor-pointer"
+            onClick={() => toggleMenu()}
+          />
+          {isOn ? (
+            <div className="h-100 bg-inherit">
+              <Link to={"/about-us"} className="text-white">
+                Chúng tôi
+              </Link>
+            </div>
+          ) : null}
+        </div>
+        <Link to={"/"} className="text-4xl font-bold cursor-pointer">
           FenFilm
         </Link>
         <div className="ml-auto hidden md:block">
@@ -25,21 +37,7 @@ const NavBar = () => {
           </Link>
         </div>
       </div>
-      {/* Menu for small screen */}
-      <div className="block ml-[5%] md:hidden mt-4">
-        <RiMenu3Line
-          size={40}
-          className="text-white cursor-pointer"
-          onClick={() => toggleMenu()}
-        />
-        {isOn ? (
-          <div className="h-100 bg-inherit">
-            <Link to={"/about-us"} className="text-white">
-              Chúng tôi
-            </Link>
-          </div>
-        ) : null}
-      </div>
+
       {/* White line */}
       <div className="bg-white h-[1px] mt-[1%] mx-[5%]"></div>
     </div>
