1bf4109e031b46d4549ed98c8250b8ebb7eec9e2
rigby200602 <tungle3456@gmail.com>
2025-10-12 23:07:12 +0700

    format

--- frontEnd/src/components/SideBar.jsx
+++ frontEnd/src/components/SideBar.jsx
@@ -7,23 +7,25 @@ const SideBar = () => {
   const toggleMenu = () => {
     setIsOn(!isOn);
   };
-  return <div>
-    {/* Menu for small screen */}
-        <div className="absolute ml-[5%] left-0 md:hidden">
-          <RiMenu3Line
-            size={40}
-            className="text-white cursor-pointer"
-            onClick={() => toggleMenu()}
-          />
-          {isOn ? (
-            <div className="h-100 bg-inherit">
-              <Link to={"/about-us"} className="text-white">
-                Chúng tôi
-              </Link>
-            </div>
-          ) : null}
-        </div>
-  </div>;
+  return (
+    <div>
+      {/* Menu for small screen */}
+      <div className="absolute ml-[5%] left-0 md:hidden">
+        <RiMenu3Line
+          size={40}
+          className="text-white cursor-pointer"
+          onClick={() => toggleMenu()}
+        />
+        {isOn ? (
+          <div className="h-100 bg-inherit">
+            <Link to={"/about-us"} className="text-white">
+              Chúng tôi
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
