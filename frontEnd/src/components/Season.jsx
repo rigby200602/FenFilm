4ac8b310b447cef9bf536432fc9fad119920e4a0
rigby200602 <tungle3456@gmail.com>
2025-10-15 22:02:15 +0700

    update css

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -23,8 +23,8 @@ const Season = ({ data }) => {
             to={`/play/${item.id}`}
             key={id}
             onClick={()=> scrollTo(0,0)}
-            className="h-10 cursor-pointer py-[5%] mx-auto w-16 sm:w-20 md:w-18 lg:w-30
-          hover:border hover:border-white hover:rounded-2xl hover:text-black hover:bg-white transition
+            className="h-10 cursor-pointer py-[5%] mx-auto w-full max-w-22 sm:max-w-24 md:max-w-22 lg:max-w-28
+          hover:border hover:border-white hover:rounded-2xl hover:text-black hover:bg-white
           // for reponsive
           border border-white rounded-2xl text-black bg-white
           lg:border-0 lg:rounded-none lg:text-white lg:bg-transparent
