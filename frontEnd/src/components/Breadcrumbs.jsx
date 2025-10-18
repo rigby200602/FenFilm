27f2c327905aa718fd38f96ff8c06dc8d49fb85b
rigby200602 <tungle3456@gmail.com>
2025-10-18 22:48:15 +0700

    update size icon

--- frontEnd/src/components/Breadcrumbs.jsx
+++ frontEnd/src/components/Breadcrumbs.jsx
@@ -4,7 +4,7 @@ import { IoHome } from 'react-icons/io5';
 const Breadcrumbs = () => {
   return (
     <div className="flex flex-wrap space-x-2 text-sm text-gray-500 font-medium mx-[5%] my-4 items-center">
-            <IoHome />
+            <IoHome size='20'/>
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="m14.413 10.663-6.25 6.25a.939.939 0 1 1-1.328-1.328L12.42 10 6.836 4.413a.939.939 0 1 1 1.328-1.328l6.25 6.25a.94.94 0 0 1-.001 1.328" fill="#CBD5E1"/>
             </svg>
