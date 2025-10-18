18cacb26367d68988f386102122af79c0414c109
rigby200602 <tungle3456@gmail.com>
2025-10-18 22:54:09 +0700

    update get id, call context

--- frontEnd/src/components/Breadcrumbs.jsx
+++ frontEnd/src/components/Breadcrumbs.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { use } from 'react'
 import { IoHome } from 'react-icons/io5';
-
+import { useApp } from '../context/AppContext';
+import { useParams } from 'react-router-dom';
 const Breadcrumbs = () => {
+    // get id
+  const { id } = useParams();
+    const { navigate } = useApp();
   return (
     <div className="flex flex-wrap space-x-2 text-sm text-gray-500 font-medium mx-[5%] my-4 items-center">
-            <IoHome size='20' onClick={() => console()}/>
+            <IoHome size='20' onClick={() => navigate('/')}/>
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="m14.413 10.663-6.25 6.25a.939.939 0 1 1-1.328-1.328L12.42 10 6.836 4.413a.939.939 0 1 1 1.328-1.328l6.25 6.25a.94.94 0 0 1-.001 1.328" fill="#CBD5E1"/>
             </svg>
