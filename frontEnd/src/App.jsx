5639a9bf8ab9dc04294439e3c966b73d92cdecbe
rigby200602 <tungle3456@gmail.com>
2025-10-20 20:54:52 +0700

    add back strictmode

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,27 +1,33 @@
-import Carousel from "./components/Carousel"
-import Footer from "./components/Footer"
-import ListCartoon from "./components/ListCartoon"
-import NavBar from "./components/NavBar"
-import {slides} from "./data/slides.json"
-import lists from './data/lists.json'
-import collections from './data/collections.json'
-import { AppContextProvider } from "./context/AppContext"
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import ListCartoon from "./components/ListCartoon";
+import NavBar from "./components/NavBar";
+import { slides } from "./data/slides.json";
+import lists from "./data/lists.json";
+import collections from "./data/collections.json";
+import { AppContextProvider } from "./context/AppContext";
+import React from "react";
 
 function App() {
-
   return (
-    <AppContextProvider>
-      <NavBar />
-      <Carousel data={slides}/>
-      {collections.map((item, i) => {
-        return (
-          <ListCartoon key={i} title={item.name} data={lists} collection={item.id}/>
-        )
-      })
-      }
-      <Footer />
-    </AppContextProvider>
-  )
+    <React.StrictMode>
+      <AppContextProvider>
+        <NavBar />
+        <Carousel data={slides} />
+        {collections.map((item, i) => {
+          return (
+            <ListCartoon
+              key={i}
+              title={item.name}
+              data={lists}
+              collection={item.id}
+            />
+          );
+        })}
+        <Footer />
+      </AppContextProvider>
+    </React.StrictMode>
+  );
 }
 
-export default App
+export default App;
