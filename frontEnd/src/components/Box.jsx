029a76c9c8ca6679fb568d25ddea451808a98c58
rigby200602 <tungle3456@gmail.com>
2025-10-21 22:46:07 +0700

    update tities

--- frontEnd/src/components/Box.jsx
+++ frontEnd/src/components/Box.jsx
@@ -6,6 +6,7 @@ const Box = ( {data} ) => {
     <Link to={`/play/${data.id}`}
     className="text-white mt-4 max-w-75 md:max-w-78 lg:max-w-80 cursor-pointer transition hover:scale-105">
               <img src={data.thumnail}
+              title={`${data.title}`}
               loading='lazy' />
               <h1 className="text-start my-2">{data.title}</h1>
     </Link>
