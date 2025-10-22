d9dd1cf13908761a5a49fae1b1ce56380d2d7c97
rigby200602 <tungle3456@gmail.com>
2025-10-22 22:17:18 +0700

    update listCartoon

--- frontEnd/src/components/ListCartoon.jsx
+++ frontEnd/src/components/ListCartoon.jsx
@@ -4,29 +4,27 @@ import { Link } from "react-router-dom";
 
 const ListCartoon = (props) => {
   // get collection
-  const collection = props.collection
+  const collection = props.collection;
   // get all data from the collection
-  const list = props.data.filter(i => i.collection === collection);
+  const list = props.data.filter((i) => i.collection === collection);
   // only show 4 box
   const data = list.filter((item) => item.id < 5);
   return (
     <div className="h-[15%] mx-[5%] mt-8">
       {data.length > 0 ? (
-      <>
-      <Link to={`/collections/${props.collection}`}>
-        <h1 className="text-3xl text-white text-center md:text-start">
-          {props.title}
-        </h1>
-      </Link>
-      {/* Show box */}
-      {/* Check if data have any attribute, only render if have any */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {data.map((item, i) => {
-            return <Box key={i} data={item} />;
-          })}
+        <div>
+          <h1 className="text-3xl text-white text-center md:text-start">
+            <Link to={`/collections/${props.collection}`}>{props.title}</Link>
+          </h1>
+          {/* Show box */}
+          {/* Check if data have any attribute, only render if have any */}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {data.map((item, i) => {
+              return <Box key={i} data={item} />;
+            })}
+          </div>
         </div>
-      </>
-      ) : null }
+      ) : null}
     </div>
   );
 };
