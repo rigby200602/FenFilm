6778768d0470b36f5cfdb9a5c147e6baaf1d3578
rigby200602 <tungle3456@gmail.com>
2025-10-24 23:16:53 +0700

    add state

--- frontEnd/src/components/SearchBar.jsx
+++ frontEnd/src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useState } from 'react';
 import { CiSearch } from "react-icons/ci";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState()
   return (
     <div className='flex bg-white text-black rounded-4xl mx-2 justify-between items-center'>
         <span>
             <CiSearch className='mx-2'size={20}/>
         </span>
-        <input type='text' placeholder='Tìm kiếm' className='outline-none pr-4'/>
+        <input type='text' placeholder='Tìm kiếm' className='outline-none pr-4' onChange={() => setSearch()}/>
     </div>
   )
 }
