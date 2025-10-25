b21822ef047a60f6666cd99d459da717dd878ee7
rigby200602 <tungle3456@gmail.com>
2025-10-25 22:44:20 +0700

    add to SideBar

--- frontEnd/src/components/SideBar.jsx
+++ frontEnd/src/components/SideBar.jsx
@@ -4,6 +4,7 @@ import { RiMenu3Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
+import SearchBar from "./SearchBar";
 
 const SideBar = () => {
   const [isOn, setIsOn] = useState(false);
@@ -11,9 +12,9 @@ const SideBar = () => {
     setIsOn(!isOn);
   };
   return (
-    <div className="block mr-auto md:hidden">
+    <div className="flex md:hidden w-full">
       {/* Menu for small screen */}
-      <div className="">
+      <div className="mr-auto">
         <RiMenu3Line
           size={40}
           className="text-white cursor-pointer"
@@ -24,19 +25,22 @@ const SideBar = () => {
             {/* Home page */}
             <Link to={"/"} className="text-white">
               <span className="flex items-center my-2">
-                <IoHome className="mr-2"/>
+                <IoHome className="mr-2" />
                 Trang chủ
               </span>
             </Link>
             {/* About us page */}
             <Link to={"/about-us"} className="text-white">
               <span className="flex items-center my-2">
-                <FaUserAlt className="mr-2"/>
+                <FaUserAlt className="mr-2" />
                 Chúng tôi
               </span>
             </Link>
           </div>
         ) : null}
+      </div>
+      <div className="flex md:hidden justify-center items-center my-4 mx-[5%] w-full max-w-60 ml-auto top-0">
+        <SearchBar />
       </div>
     </div>
   );
