d07df86f431c07302b80ffc6afcfdb30f6547417
rigby200602 <tungle3456@gmail.com>
2025-10-30 22:02:53 +0700

    update css

--- frontEnd/src/components/Pagination.jsx
+++ frontEnd/src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import { GrLinkPrevious } from "react-icons/gr";
 
 const Pagination = () => {
   return (
-    <div className='flex items-center gap-2'>
+    <div className='flex items-center gap-2 mx-[5%] my-4'>
         <button className='w-full rounded-4xl'>
             <GrLinkPrevious />
         </button>
