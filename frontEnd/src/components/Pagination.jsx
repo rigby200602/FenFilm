5840070dac402756e6a58186657c8e3f6da6ed06
rigby200602 <tungle3456@gmail.com>
2025-10-30 22:00:17 +0700

    add prev button

--- frontEnd/src/components/Pagination.jsx
+++ frontEnd/src/components/Pagination.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { GrLinkPrevious } from "react-icons/gr";
 
 const Pagination = () => {
   return (
     <div className='flex items-center gap-2'>
-
+        <button>
+            <GrLinkPrevious />
+        </button>
     </div>
   )
 }
