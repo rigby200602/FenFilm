2e8037b9c3649cd012db03a66997dacbf0abc7fa
rigby200602 <tungle3456@gmail.com>
2025-11-01 01:46:46 +0700

    update css

--- frontEnd/src/components/SearchBar.jsx
+++ frontEnd/src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
     console.log(search)
   }
   return (
-    <div className='flex bg-white text-black rounded-l-4xl  md:mr-0 ml-0 md:ml-2 justify-between items-center w-full max-w-90 md:max-w-60 h-10'>
+    <div className='flex bg-white text-black rounded-4xl  md:mr-0 ml-0 md:ml-2 justify-between items-center w-full max-w-90 md:max-w-60 h-10'>
         <span>
             <CiSearch className='mx-2'size={20}/>
         </span>
