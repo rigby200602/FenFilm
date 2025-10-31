5c01c5fae59f20e992ba6b72443c9a0ad29b969a
rigby200602 <tungle3456@gmail.com>
2025-11-01 01:29:14 +0700

    add pointer

--- frontEnd/src/components/Pagination.jsx
+++ frontEnd/src/components/Pagination.jsx
@@ -4,10 +4,10 @@ import { GrLinkPrevious, GrLinkNext } from "react-icons/gr";
 const Pagination = () => {
   return (
     <div className='flex justify-center items-center gap-2 mx-[5%] my-4'>
-        <button className='w-10 h-10 rounded-full bg-white text-black'>
+        <button className='w-10 h-10 rounded-full bg-white text-black cursor-pointer'>
             <GrLinkPrevious className='mx-auto'/>
         </button>
-        <button className='w-10 h-10 rounded-full bg-white text-black'>
+        <button className='w-10 h-10 rounded-full bg-white text-black cursor-pointer'>
             <GrLinkNext className='mx-auto'/>
         </button>
     </div>
