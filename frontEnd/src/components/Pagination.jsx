5ed6f1137c3bb0b2b8f7df481529819617978390
rigby200602 <tungle3456@gmail.com>
2025-11-01 01:44:09 +0700

    update css

--- frontEnd/src/components/Pagination.jsx
+++ frontEnd/src/components/Pagination.jsx
@@ -4,7 +4,7 @@ import { GrLinkPrevious, GrLinkNext } from "react-icons/gr";
 const Pagination = () => {
   return (
     <div className="flex justify-center items-center gap-2 mx-[5%] my-4">
-      <button className="w-10 h-10  hover:bg-gray-100/70 transition-all text-black cursor-pointer">
+      <button className="w-10 h-10 bg-white border border-gray-200 rounded-full hover:bg-gray-100/70 transition-all text-black cursor-pointer">
         <GrLinkPrevious className="mx-auto" />
       </button>
       <div class="flex gap-2 text-gray-500 text-sm md:text-base">
@@ -45,7 +45,7 @@ const Pagination = () => {
           6
         </button>
       </div>
-      <button className="w-10 h-10 rounded-full bg-white text-black cursor-pointer">
+      <button className="w-10 h-10 bg-white border border-gray-200 rounded-full hover:bg-gray-100/70 transition-all text-black cursor-pointer">
         <GrLinkNext className="mx-auto" />
       </button>
     </div>
