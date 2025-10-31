7820e2d8d1aeac7bf8d294d8c4a462783717f8ba
rigby200602 <tungle3456@gmail.com>
2025-11-01 01:49:01 +0700

    update margin

--- frontEnd/src/components/SearchBar.jsx
+++ frontEnd/src/components/SearchBar.jsx
@@ -10,11 +10,11 @@ const SearchBar = () => {
     console.log(search)
   }
   return (
-    <div className='flex bg-white text-black rounded-4xl  md:mr-0 ml-0 md:ml-2 justify-between items-center w-full max-w-90 md:max-w-60 h-10'>
+    <div className='flex bg-white text-black rounded-4xl md:mr-0 ml-0 md:ml-2 justify-between items-center w-full max-w-90 md:max-w-60 h-10'>
         <span>
             <CiSearch className='mx-2'size={20}/>
         </span>
-        <input name='Search' type='text' placeholder='Tìm kiếm' className='outline-none pr-4' onChange={handleSearch}/>
+        <input name='Search' type='text' placeholder='Tìm kiếm' className='outline-none mr-4' onChange={handleSearch}/>
     </div>
   )
 }
