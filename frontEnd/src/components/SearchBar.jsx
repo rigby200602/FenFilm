1b5081f1a64a2f79346498ba9001e043142f2d27
rigby200602 <tungle3456@gmail.com>
2025-11-03 21:40:32 +0700

    handleFocus

--- frontEnd/src/components/SearchBar.jsx
+++ frontEnd/src/components/SearchBar.jsx
@@ -9,6 +9,9 @@ const SearchBar = () => {
     setSearch(e.target.value)
     console.log(search)
   }
+  const handleFocus = () => {
+    stopPropagation();
+  }
   return (
     <div className='flex bg-white text-black rounded-4xl md:mr-0 ml-0 md:ml-2 justify-between items-center w-full max-w-90 md:max-w-60 h-10'>
         <span>
