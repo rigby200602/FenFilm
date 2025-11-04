a0fb2633f8056847a8fe56b2d856e9227aea5376
rigby200602 <tungle3456@gmail.com>
2025-11-04 21:16:27 +0700

    css for active link

--- frontEnd/src/components/Season.jsx
+++ frontEnd/src/components/Season.jsx
@@ -29,6 +29,7 @@ const Season = ({ data }) => {
             onClick={handleClick}
             className="h-10 cursor-pointer py-[5%] mx-auto w-full max-w-24 sm:max-w-24 md:max-w-22 lg:max-w-28
           hover:border hover:border-white hover:rounded-2xl hover:text-black hover:bg-white
+          active:bg-white active:text-black
           // for reponsive
           border border-white rounded-2xl text-black bg-white
           lg:border-0 lg:rounded-none lg:text-white lg:bg-transparent
